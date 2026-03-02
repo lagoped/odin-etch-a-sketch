e24babe95b962480ad3980be7791b142fa8edc2d
lagopede <lagopede334@hotmail.fr>
2026-03-02 12:22:38 -0500

    WIP first draft of grid

---
+++ canvas.js
@@ -0,0 +1,60 @@
+const defaultGridSize = 16;
+
+let gridSize = defaultGridSize;
+
+
+
+function createGrid(){
+    for(let colIndex = 0; colIndex <= gridSize; colIndex++) {
+        createGridRow(colIndex)
+    }
+}
+
+function createGridRow(colIndex) {
+    let canvas = document.getElementById("canvas");
+    const currentRowTest = createGridRowElement(colIndex);
+    console.log(currentRowTest);
+
+    for(let rowIndex = 0; rowIndex <= gridSize; rowIndex++) {
+        let square = createGridSquareElement(colIndex, rowIndex)
+        currentRowTest.append(square);
+    }
+
+    canvas.append(currentRowTest);
+}
+
+
+
+/**
+ * Factory
+ */
+function createGridRowElement(colIndex) {
+    let row = document.createElement("div");
+    row.id = getCurrentColumnId(colIndex);
+    row.classList.add("canvas-row");
+
+    return row;
+}
+
+
+function getCurrentColumnId(colIndex){
+    return "canvas-column-" + colIndex;
+}
+
+
+
+function createGridSquareElement(colIndex, rowIndex) {
+    let square = document.createElement("div");
+    square.id = getCurrentSquareId(colIndex, rowIndex);
+    square.classList.add("canvas-square");
+
+    return square;
+}
+
+
+function getCurrentSquareId(colIndex, rowIndex) {
+    return "square-col-" + colIndex + "-row-" + rowIndex; 
+}
+
+
+createGrid();
